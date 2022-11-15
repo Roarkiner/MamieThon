f4d6e16dd22e4381d872fd395f7aa56c4d57b49d
L.D <61159151+Roarkiner@users.noreply.github.com>
2022-11-15 22:19:18 +0100

    First commit, Firebased installed

---
+++ app/src/main/java/com/example/mamiethon/LoginActivity.kt
@@ -0,0 +1,28 @@
+package com.example.mamiethon
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = Firebase.auth
+        val currentUser = auth.currentUser
+        if(currentUser == null)
+            setContentView(R.layout.activity_login)
+        else{
+            switchToRecipeListActivity()
+        }
+    }
+
+    private fun switchToRecipeListActivity(){
+        val switchActivityIntent = Intent(this, RecipyList::class.java)
+        startActivity(switchActivityIntent)
+    }
+}
