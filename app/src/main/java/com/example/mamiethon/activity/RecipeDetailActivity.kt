60a222ee9f6b7918b4c6b93602c75eef2e394b4c
L.D <61159151+Roarkiner@users.noreply.github.com>
2023-02-27 22:48:52 +0100

    Html instructions display

--- app/src/main/java/com/example/mamiethon/activity/RecipeDetailActivity.kt
+++ app/src/main/java/com/example/mamiethon/activity/RecipeDetailActivity.kt
@@ -1,13 +1,11 @@
 package com.example.mamiethon.activity
 
 import android.content.Intent
-import android.graphics.ColorFilter
-import android.opengl.Visibility
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.HtmlCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
@@ -15,11 +13,8 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mamiethon.R
 import com.example.mamiethon.adapter.IngredientListAdapter
-import com.example.mamiethon.adapter.RecipeListAdapter
 import com.example.mamiethon.adapter.SimilarRecipeListAdapter
-import com.example.mamiethon.data.LightRecipe
 import com.example.mamiethon.data.SimilarRecipe
-import com.example.mamiethon.interfaces.IRecipeListClickListener
 import com.example.mamiethon.interfaces.ISimilarRecipeListClickListener
 import com.example.mamiethon.viewModel.RecipeDetailViewModel
 import com.squareup.picasso.Picasso
@@ -71,7 +66,8 @@ class RecipeDetailActivity : AppCompatActivity() {
                 recipeVeganImageView.alpha = 0.2f
             }
 
-            recipeInstructionsView.text = it.instructions
+            val htmlFormattedInstructions = HtmlCompat.fromHtml(it.instructions, HtmlCompat.FROM_HTML_MODE_COMPACT)
+            recipeInstructionsView.text = htmlFormattedInstructions
         })
 
         val similarRecipeRecyclerView = findViewById<RecyclerView>(R.id.recipe_similar_recipes)
